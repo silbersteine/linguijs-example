0a12bde10450355cb77c1f8926f666bbfd77f743
silbersteine13 <108279588+silbersteine13@users.noreply.github.com>
2023-01-18 13:58:16 -0600

    add i18n wrapper

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+
+import { i18n } from '@lingui/core'
+import { I18nProvider } from '@lingui/react'
+import { messages } from './locales/en/messages'
 import Inbox from './Inbox'
 
-const App = () => <Inbox />
+
+i18n.load('en', messages)
+i18n.activate('en')
+
+const App = () => (
+  <I18nProvider i18n={i18n}>
+    <Inbox />
+  </I18nProvider>
+)
 
 render(<App />, document.getElementById('root'))
